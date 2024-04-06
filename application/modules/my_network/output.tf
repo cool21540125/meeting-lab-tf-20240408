0732c67e726b30a990af9d0343ff0c726d65f329
Tony Chou <cool21540125@gmail.com>
2024-04-06 19:15:29 +0800

    impl: add out_vpc_id in my_network for others to use

--- application/modules/my_network/output.tf
+++ application/modules/my_network/output.tf
@@ -5,3 +5,7 @@ output "out_public_subnets_cidr" {
 output "out_private_subnets_cidr" {
   value = [for s in aws_subnet.my_private_subnets : s.cidr_block]
 }
+
+output "out_vpc_id" {
+  value = aws_vpc.my_vpc.id
+}
