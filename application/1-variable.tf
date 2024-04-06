77139b0ee291ac211ef4d9be87a674dcad1cf4b4
Tony Chou <cool21540125@gmail.com>
2024-04-06 21:03:36 +0800

    impl: create security group for dmz & private subnets

--- application/1-variable.tf
+++ application/1-variable.tf
@@ -3,3 +3,44 @@ variable "aws_region" {
   type    = string
   default = "ap-northeast-1"
 }
+
+variable "my_ip" {
+  type    = string
+  default = ""
+}
+
+locals {
+  dmz_sg_ingress_rules = {
+    ingress = {
+      "http-public-subnet" = {
+        cidr_ipv4   = "${var.my_ip}/32"
+        ip_protocol = "tcp"
+        from_port   = 80
+        to_port     = 80
+      }
+      "https-public-subnet" = {
+        cidr_ipv4   = "${var.my_ip}/32"
+        ip_protocol = "tcp"
+        from_port   = 443
+        to_port     = 443
+      }
+      "ssh-public-subnet" = {
+        cidr_ipv4   = "${var.my_ip}/32"
+        ip_protocol = "tcp"
+        from_port   = 22
+        to_port     = 22
+      }
+    }
+  }
+  private_sg_ingress_rules = {
+    ingress = {
+      "from-dmz-sg" = {
+        referenced_security_group_id = module.my_dmz_sg.out_security_group.id
+        ip_protocol                  = "tcp"
+        from_port                    = 22
+        to_port                      = 22
+      }
+    }
+  }
+}
+
--- application/2-main.tf
+++ application/2-main.tf
@@ -2,3 +2,20 @@ module "my_network" {
   source              = "./modules/my_network"
   network_config_path = "./config/my_network.yaml"
 }
+
+module "my_dmz_sg" {
+  source           = "./modules/my_network_security"
+  vpc_id           = module.my_network.out_vpc_id
+  sg_ingress_rules = local.dmz_sg_ingress_rules
+
+  sg_usage = "dmz_sg"
+}
+
+module "my_private_sg" {
+  source           = "./modules/my_network_security"
+  vpc_id           = module.my_network.out_vpc_id
+  sg_ingress_rules = local.private_sg_ingress_rules
+  depends_on       = [module.my_dmz_sg]
+
+  sg_usage = "private_sg"
+}
