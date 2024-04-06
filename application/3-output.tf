08c3f89adb36dbdd56dfe1712b1100319abb1bbd
Tony Chou <cool21540125@gmail.com>
2024-04-06 13:42:18 +0800

    impl: show output from module

--- application/3-output.tf
+++ application/3-output.tf
@@ -1,1 +1,7 @@
+output "out_public_subnets_cidr" {
+  value = module.my_network.out_public_subnets_cidr
+}
 
+output "out_private_subnets_cidr" {
+  value = module.my_network.out_private_subnets_cidr
+}
